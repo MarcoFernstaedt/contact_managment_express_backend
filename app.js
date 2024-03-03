73ed749777877a809979897cefc5ba4c80d631aa
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-02 22:42:13 -0700

    connected to database

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require('mongoose')
 const router = require("./routes");
 const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/contact_managment_db")
 
 app.use(express.json());
 
