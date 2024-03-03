476fdb67ce31f11fa8babbfddd3b746dc0e1c270
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-02 20:15:10 -0700

    create test controllers for create update and delete contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,3 +1,15 @@
 module.exports.getContacts = (req, res) => {
   res.status(200).json({ message: "success!" });
 };
+
+module.exports.createContact = (req, res) => {
+  res.status(201).json({ message: "created contact!" });
+};
+
+module.exports.updateContact = (req, res) => {
+  res.status(200).json({ message: `update contact ${req.params.id}` });
+};
+
+module.exports.deleteContact = (req, res) => {
+  res.status(200).json({ message: `delete contact ${req.params.id}` });
+};
