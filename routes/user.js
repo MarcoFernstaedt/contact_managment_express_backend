105353df14a0dfc5b8d371bbe31cf8bce8cee42d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-05 13:37:04 -0700

    create: routes for create update delete and get user

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const {
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user");
+
+router.get("/:userId", getUser);
+router.post("/", createUser);
+router.put("/:userId", updateUser);
+router.delete("/:userId", deleteUser);
+
+module.exports = router;
