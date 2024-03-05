c7c81b90dd172d135bd42d8ad0e6f26f73480f3d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-04 19:09:10 -0700

    create: routes for creating updating deleting finding specified contact and get all contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const {
   getContacts,
+  getContact,
   createContact,
   updateContact,
   deleteContact,
 } = require("../controllers/contacts");
 
 router.get("/", getContacts);
+router.get('/:contactId', getContact);
 router.post("/", createContact);
 router.put("/:contactId", updateContact);
 router.delete("/:contactId", deleteContact);
