9b56cb3acbea22af84e7916c3cbb3fad855d95ea
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-12 14:45:13 -0700

    refactor: model no contains user_id

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,39 +1,42 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const contactSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      minlength: 2,
-      maxlength: 30,
-      required: [true, "Please add the contact name"],
-    },
-    email: {
-      type: String,
-      minlength: 2,
-      maxlength: 100,
-      unique: true,
-      validate: {
-        validator(value) {
-          return validator.isEmail(value);
-        },
-        message: "Yout must enter a valid Email address",
-      },
-      required: [true, "Please add the contact email"],
-    },
-    phone: {
-      type: String,
-      minlength: 10,
-      maxlength: 15,
-      unique: true,
-      required: [true, "Please add the contact phone"],
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+const contactSchema = mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
   },
-);
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: [true, "Please add the contact name"],
+  },
+  email: {
+    type: String,
+    minlength: 2,
+    maxlength: 100,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "Yout must enter a valid Email address",
+    },
+    required: [true, "Please add the contact email"],
+  },
+  phone: {
+    type: String,
+    minlength: 10,
+    maxlength: 15,
+    unique: true,
+    required: [true, "Please add the contact phone"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model('Contact', contactSchema)
+module.exports = mongoose.model("Contact", contactSchema);
