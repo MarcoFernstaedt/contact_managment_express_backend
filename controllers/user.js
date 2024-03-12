06b28acdd2e3f499538346a0eef8374fe318ce73
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-12 14:40:30 -0700

    implament register login and currentUser controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,72 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 // @description Register a user
 // @route POST /user/register
 // @access Public
-const registerUser = (req, res) => {
-  
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    res.status(201).json({ _id: user._id, email: user.email });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
 };
 
 // @description Login users
 // @route POST /user/login
 // @access Public
-const loginUser = (req, res) => {
-  // login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const payload = {
+      user: {
+        name: user.name,
+        email: user.email,
+        id: user.id,
+      },
+    };
+
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({ accessToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
 };
 
-// @description get current user info
+// @description Get current user info
 // @route GET /user/current
 // @access Private
 const currentUser = (req, res) => {
-  // current user info
+  res.json(req.user)  
 };
 
-module.exports = { registerUser, loginUser, getComputedStyle };
+module.exports = { registerUser, loginUser, currentUser };
