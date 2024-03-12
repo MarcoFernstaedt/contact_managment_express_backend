a8e6b56439757c4103688e976a4567b6d312a552
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-11 22:34:54 -0700

    uncommented userRoute

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,19 @@
 const router = require("express").Router();
 const {
+  registerUser,
+  loginUser,
   getUser,
   createUser,
   updateUser,
   deleteUser,
 } = require("../controllers/user");
 
-router.get("/:userId", getUser);
-router.post("/", createUser);
-router.put("/:userId", updateUser);
-router.delete("/:userId", deleteUser);
+router.post("/register", registerUser);
+router.post('/login', loginUser);
+router.get('/current', currentUser)
+// router.get("/:userId", getUser);
+// router.post("/", createUser);
+// router.put("/:userId", updateUser);
+// router.delete("/:userId", deleteUser);
 
 module.exports = router;
