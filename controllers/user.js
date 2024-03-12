7d0e4d0b86e17a149329b9ace64145173542a83d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-11 22:33:32 -0700

    implament bcryptjs for password hashing created register controller

---
+++ controllers/user.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+// @description Register a user
+// @route POST /user/register
+// @access Public
+const registerUser = (req, res) => {
+  
+};
+
+// @description Login users
+// @route POST /user/login
+// @access Public
+const loginUser = (req, res) => {
+  // login
+};
+
+// @description get current user info
+// @route GET /user/current
+// @access Private
+const currentUser = (req, res) => {
+  // current user info
+};
+
+module.exports = { registerUser, loginUser, getComputedStyle };
